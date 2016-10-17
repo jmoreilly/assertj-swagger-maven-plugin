830310b8d2f2c45a5d371c1191b3641fd8112c42
James O'Reilly <James.OReilly@macquarie.com>
2016-10-17 22:27:24 +1100

    initial commit

---
+++ src/main/java/au/com/oreilly/assertjswagger/AssertMojo.java
@@ -0,0 +1,91 @@
+package au.com.oreilly.assertjswagger;
+
+
+import io.github.robwin.swagger.test.SwaggerAssert;
+import io.github.robwin.swagger.test.SwaggerAssertionConfig;
+import io.github.robwin.swagger.test.SwaggerAssertionType;
+import io.github.robwin.swagger.test.SwaggerAssertions;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.assertj.core.api.SoftAssertionError;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
+@Mojo(name="assert")
+public class AssertMojo extends AbstractMojo {
+
+    @Parameter
+    private String contract;
+
+    @Parameter
+    private String actual;
+
+    @Parameter(defaultValue = "false")
+    private boolean assertEqual;
+
+    @Parameter(defaultValue = "true")
+    private boolean failOnError;
+
+    private Swagger parse(String location) {
+        SwaggerParser parser = new SwaggerParser();
+        try {
+            URL url = new URL(location);
+            InputStream in = url.openStream();
+            return parser.parse(IOUtils.toString(in));
+        }
+        catch (MalformedURLException e) {
+            File f = new File(location);
+            return parser.read(f.toString());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Properties props = new Properties();
+        props.setProperty("assertj.swagger." + SwaggerAssertionType.INFO.getBarePropertyName(), "true");
+        props.setProperty("assertj.swagger." + SwaggerAssertionType.VERSION.getBarePropertyName(), "true");
+        SwaggerAssertionConfig config = new SwaggerAssertionConfig(props);
+        SwaggerAssert asserter = new SwaggerAssert(parse(actual), config);
+
+        String test = "";
+        try {
+            Swagger contractSpec = parse(contract);
+
+            if (assertEqual) {
+                test = "in not equal to";
+                asserter.isEqualTo(contractSpec);
+                test = "is equal to";
+            } else {
+                test = "does not satisfy";
+                asserter.satisfiesContract(contractSpec);
+                test = "satisfies";
+            }
+            getLog().info(actual.toString() + " " + test + " " + contract.toString());
+        }
+        catch (SoftAssertionError e) {
+            throw new MojoFailureException(actual.toString() + " " + test + " " + contract.toString(), e);
+        }
+        catch (Exception e) {
+            getLog().error(e);
+            if (failOnError) {
+                throw new MojoExecutionException(e.getMessage());
+            }
+        }
+    }
+}
